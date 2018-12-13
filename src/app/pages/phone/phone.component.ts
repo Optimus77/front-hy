0eb0682a95b095d98e4c95f02b0bd5acccd507e0
Optimus77 <Optimus_7@163.com>
2018-12-13 16:39:18 +0800

    photo

--- src/app/pages/phone/phone.component.ts
+++ src/app/pages/phone/phone.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+// @ts-ignore
+// @ts-ignore
+// @ts-ignore
 @Component({
   selector: 'ngx-phone',
   templateUrl: './phone.component.html',
@@ -9,18 +12,50 @@ export class PhoneComponent implements OnInit {
 
   constructor() { }
   tag;
-  aaa = "./1.png";
+  selectedCamera;
+
+  cameras: any[] = [{
+    title: 'Camera #1',
+    source: 'assets/images/camera1.jpg',
+  }, {
+    title: 'Camera #2',
+    source: 'assets/images/camera2.jpg',
+  }, {
+    title: 'Camera #3',
+    source: 'assets/images/camera3.jpg',
+  }, {
+    title: 'Camera #4',
+    source: 'assets/images/camera4.jpg',
+  }];
+
+
+
+  userMenu = [{
+    title: 'Profile',
+  }, {
+    title: 'Log out',
+  }];
+
+  isSingleView = true;
+
+  selectCamera(camera: any) {
+    this.selectedCamera = camera;
+    this.isSingleView = false;
+  }
+
   left_Click() {
     this.tag--;
+    this.selectedCamera = this.cameras[this.tag];
   }
 
   right_Click() {
     this.tag++;
+    this.selectedCamera = this.cameras[this.tag];
   }
 
-
   ngOnInit() {
-    this.tag=1
+    this.tag=0;
+    this.selectedCamera= this.cameras[0];
   }
 
 }
